fcb7a63f560fe9acf2ceeb898a81adb698012998
Chris <Christopher.R.Couture@gmail.com>
2016-10-06 23:57:57 -0400

    Initial Load

---
+++ SkillPrestigeCookingAdapter/CookingSkillMod.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SkillPrestige;
+using SkillPrestige.Mods;
+using SkillPrestige.Professions;
+using SkillPrestige.SkillTypes;
+using StardewValley;
+
+namespace SkillPrestigeCookingAdapter
+{
+    public class CookingSkillMod : SkillMod
+    {
+        public override string DisplayName => "Cooking Skill";
+        protected override string Namespace => "CookingSkill";
+        protected override string ClassName => "CookingSkillMod";
+        public override IEnumerable<Skill> AdditionalSkills => IsFound ?
+        new List<Skill>
+            {
+                new Skill
+                {
+                    Type = CookingSkillType,
+                    SkillScreenPosition = Type.GetType("LuckSkill.LuckSkillMod, LuckSkill") == null ? 6 : 7, //variable due to potential conflict with order due to luck skill mod.
+                    SourceRectangleForSkillIcon = new Rectangle(0, 0, 16, 16),
+                    SkillIconTexture = SkillPrestigeCookingAdapterMod.GetCookingSkillTexture(),
+                    Professions = GetCookingProfessions(),
+                    SetSkillLevel = x => {}, //No set necessary, as the level is not stored independently from the experience.
+                    GetSkillLevel = GetCookingLevel
+                }
+            } : null;
+
+        public override IEnumerable<Prestige> AdditonalPrestiges => IsFound ? new List<Prestige>
+        {
+            new Prestige
+            {
+                SkillType = CookingSkillType
+            }
+        } : null;
+
+        private static SkillType CookingSkillType => _cookingSkillType ?? (_cookingSkillType = new SkillType("Cooking", 6));
+
+        private static SkillType _cookingSkillType;
+
+        private static int GetCookingLevel()
+        {
+            FixExpLength();
+            for (var index = ExpNeededForLevel.Length - 1; index >= 0; --index)
+            {
+                if (Game1.player.experiencePoints[6] >= ExpNeededForLevel[index])
+                    return index + 1;
+            }
+            return 0;
+        }
+
+        private static void FixExpLength()
+        {
+            if (Game1.player.experiencePoints.Length >= 7)
+                return;
+            var newExperienceArray = new int[7];
+            for (var index = 0; index < 6; ++index)
+                newExperienceArray[index] = Game1.player.experiencePoints[index];
+            Game1.player.experiencePoints = newExperienceArray;
+        }
+
+        private static readonly int[] ExpNeededForLevel =
+        {
+            100,
+            380,
+            770,
+            1300,
+            2150,
+            3300,
+            4800,
+            6900,
+            10000,
+            15000
+        };
+
+        private static List<Profession> GetCookingProfessions()
+        {
+            var gourmet = new TierOneProfession
+            {
+                Id = 50,
+                DisplayName = "Gourmet",
+                EffectText = new[] {"+20% sell price"}
+            };
+            var satisfying = new TierOneProfession
+            {
+                Id = 51,
+                DisplayName = "Satisfying",
+                EffectText = new[] {"+25% buff duration once eaten"}
+            };
+            var efficient = new TierTwoProfession
+            {
+                Id = 52,
+                DisplayName = "Efficient",
+                EffectText = new[] {"15% chance to not consume ingredients"},
+                TierOneProfession = gourmet
+            };
+            var professionalChef = new TierTwoProfession
+            {
+                Id = 53,
+                DisplayName = "Prof. Chef",
+                EffectText = new[] { "Home-cooked meals are always at least silver" },
+                TierOneProfession = gourmet
+            };
+            var heartyMeals = new TierTwoProfession
+            {
+                Id = 54,
+                DisplayName = "Home Cook",
+                EffectText = new[]
+                {
+                    "Food buffs are one level stronger",
+                    "(+20% for max energy or magnetism)"
+                },
+                TierOneProfession = satisfying
+            };
+            var sousChef = new TierTwoProfession
+            {
+                Id = 55,
+                DisplayName = "Sous Chef",
+                EffectText = new[]{"Provides a few random buffs when eating unbuffed food."},
+                TierOneProfession = satisfying
+            };
+            gourmet.TierTwoProfessions = new List<TierTwoProfession>
+            {
+                efficient,
+                professionalChef
+            };
+            satisfying.TierTwoProfessions = new List<TierTwoProfession>
+            {
+                heartyMeals,
+                sousChef
+            };
+            return new List<Profession>
+            {
+                gourmet,
+                satisfying,
+                efficient,
+                professionalChef,
+                heartyMeals,
+                sousChef
+            };
+        }
+
+    }
+}
---
+++ SkillPrestigeCookingAdapter/SkillPrestigeCookingAdapterMod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace SkillPrestigeCookingAdapter
+{
+    public class SkillPrestigeCookingAdapterMod : Mod
+    {
+        private static Texture2D _cookingSkillTexture;
+        private const string IconFilePath = @"Mods\CookingSkill\iconA.png";
+        public static Texture2D GetCookingSkillTexture()
+        {
+            if (_cookingSkillTexture != null) return _cookingSkillTexture;
+            try
+            {
+                var fileStream = new FileStream(IconFilePath, FileMode.Open);
+                _cookingSkillTexture = Texture2D.FromStream(Game1.graphics.GraphicsDevice, fileStream);
+            }
+            catch (Exception ex)
+            {
+                Log.Async("[Skill Prestige Cooking Adapter] failed to load icon: " + ex);
+                _cookingSkillTexture = new Texture2D(Game1.graphics.GraphicsDevice, 16, 16);
+                _cookingSkillTexture.SetData(Enumerable.Range(0, 256).Select(i => new Color(225, 168, byte.MaxValue)).ToArray());
+            }
+            return _cookingSkillTexture;
+        }
+    }
+}
