a353781dbb9259dbd67fd4a28eace1f20d5c39b6
Chris <Christopher.R.Couture@gmail.com>
2017-07-29 17:59:04 -0400

    updating to latest SMAPI compatability.

--- SkillPrestigeCookingAdapter/CookingSkillMod.cs
+++ SkillPrestigeCookingAdapter/CookingSkillMod.cs
@@ -1,35 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CookingSkill;
 using SkillPrestige;
+using SkillPrestige.Menus;
 using SkillPrestige.Mods;
 using SkillPrestige.Professions;
 using SkillPrestige.SkillTypes;
+using Microsoft.Xna.Framework;
 using StardewValley;
 
 namespace SkillPrestigeCookingAdapter
 {
-    public class CookingSkillMod : SkillMod
+    /// <summary>
+    /// The Cooking prestige skill mod.
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global - invoked by another application through reflection.
+    public class CookingSkillMod : ISkillMod
     {
-        public override string DisplayName => "Cooking Skill";
-        protected override string Namespace => "CookingSkill";
-        protected override string ClassName => "CookingSkillMod";
-        public override IEnumerable<Skill> AdditionalSkills => IsFound ?
-        new List<Skill>
-            {
-                new Skill
-                {
-                    Type = CookingSkillType,
-                    SkillScreenPosition = Type.GetType("LuckSkill.LuckSkillMod, LuckSkill") == null ? 6 : 7, //variable due to potential conflict with order due to luck skill mod.
-                    SourceRectangleForSkillIcon = new Rectangle(0, 0, 16, 16),
-                    SkillIconTexture = SkillPrestigeCookingAdapterMod.GetCookingSkillTexture(),
-                    Professions = GetCookingProfessions(),
-                    SetSkillLevel = x => {}, //No set necessary, as the level is not stored independently from the experience.
-                    GetSkillLevel = GetCookingLevel
-                }
-            } : null;
+        private static readonly int[] ExpNeededForLevel =
+        {
+            100,
+            380,
+            770,
+            1300,
+            2150,
+            3300,
+            4800,
+            6900,
+            10000,
+            15000
+        };
 
-        public override IEnumerable<Prestige> AdditonalPrestiges => IsFound ? new List<Prestige>
+        public string DisplayName => "Cooking Skill";
+
+        public bool IsFound => SkillPrestigeCookingAdapterMod.IsCookingSkillModLoaded;
+
+
+        public IEnumerable<Skill> AdditionalSkills => GetSkills();
+        
+        private IEnumerable<Skill> GetSkills()
+        {
+            {
+                if (IsFound)
+                {
+                    yield return new Skill
+
+                    {
+                        Type = CookingSkillType,
+                        SkillScreenPosition = SkillPrestigeCookingAdapterMod.IsLuckSkillModLoaded ? 7 : 6, // fix potential conflict with order due to luck skill mod
+                        SourceRectangleForSkillIcon = new Rectangle(0, 0, 16, 16),
+                        SkillIconTexture = SkillPrestigeCookingAdapterMod.IconTexture,
+                        Professions = GetCookingProfessions(),
+                        SetSkillLevel = x => { }, // no set necessary, as the level isn't stored independently from the experience
+                        GetSkillLevel = GetCookingLevel,
+                        SetSkillExperience = SetCookingExperience,
+                        LevelUpManager = new LevelUpManager
+                        {
+                            MenuType = typeof(CookingLevelUpMenu),
+                            GetLevel = () => (int)(Game1.activeClickableMenu as CookingLevelUpMenu).GetInstanceField("currentLevel"),
+                            GetSkill = () => Skill.AllSkills.Single(x => x.Type == CookingSkillType),
+                            CreateNewLevelUpMenu = (skill, level) => new LevelUpMenuDecorator<CookingLevelUpMenu>(skill, level, new CookingLevelUpMenu(level),
+                                "professionsToChoose", "leftProfessionDescription", "rightProfessionDescription", CookingLevelUpMenu.getProfessionDescription)
+                        }
+                    };
+                }
+            }
+        }
+
+        public IEnumerable<Prestige> AdditonalPrestiges => IsFound ? new List<Prestige>
         {
             new Prestige
             {
@@ -54,47 +92,47 @@ namespace SkillPrestigeCookingAdapter
 
         private static void FixExpLength()
         {
-            if (Game1.player.experiencePoints.Length >= 7)
-                return;
+            if (Game1.player.experiencePoints.Length >= 7) return;
             var newExperienceArray = new int[7];
             for (var index = 0; index < 6; ++index)
                 newExperienceArray[index] = Game1.player.experiencePoints[index];
             Game1.player.experiencePoints = newExperienceArray;
         }
 
-        private static readonly int[] ExpNeededForLevel =
-        {
-            100,
-            380,
-            770,
-            1300,
-            2150,
-            3300,
-            4800,
-            6900,
-            10000,
-            15000
-        };
 
-        private static List<Profession> GetCookingProfessions()
+
+        private static void SetCookingExperience(int amount)
+        {
+            if (amount <= Game1.player.experiencePoints[6])
+            {
+                Game1.player.experiencePoints[6] = amount;
+            }
+            else
+            {
+                var addedExperience = amount - Game1.player.experiencePoints[6];
+                CookingSkill.CookingSkillMod.addCookingExp(addedExperience);
+            }
+        }
+
+        private static IEnumerable<Profession> GetCookingProfessions()
         {
             var gourmet = new TierOneProfession
             {
                 Id = 50,
                 DisplayName = "Gourmet",
-                EffectText = new[] {"+20% sell price"}
+                EffectText = new[] { "+20% sell price" }
             };
             var satisfying = new TierOneProfession
             {
                 Id = 51,
                 DisplayName = "Satisfying",
-                EffectText = new[] {"+25% buff duration once eaten"}
+                EffectText = new[] { "+25% buff duration once eaten" }
             };
             var efficient = new TierTwoProfession
             {
                 Id = 52,
                 DisplayName = "Efficient",
-                EffectText = new[] {"15% chance to not consume ingredients"},
+                EffectText = new[] { "15% chance to not consume ingredients" },
                 TierOneProfession = gourmet
             };
             var professionalChef = new TierTwoProfession
@@ -119,7 +157,7 @@ namespace SkillPrestigeCookingAdapter
             {
                 Id = 55,
                 DisplayName = "Secret Spices",
-                EffectText = new[]{"Provides a few random buffs when eating unbuffed food."},
+                EffectText = new[] { "Provides a few random buffs when eating unbuffed food." },
                 TierOneProfession = satisfying
             };
             gourmet.TierTwoProfessions = new List<TierTwoProfession>
--- SkillPrestigeCookingAdapter/SkillPrestigeCookingAdapterMod.cs
+++ SkillPrestigeCookingAdapter/SkillPrestigeCookingAdapterMod.cs
@@ -1,77 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
-using StardewValley;
-using SkillPrestige;
 
 namespace SkillPrestigeCookingAdapter
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class SkillPrestigeCookingAdapterMod : Mod
     {
-        private static Texture2D _cookingSkillTexture;
-        private const string IconFileName = @"iconA.png";
-        public static Texture2D GetCookingSkillTexture()
-        {
-            if (_cookingSkillTexture != null) return _cookingSkillTexture;
-            try
-            {
-                var cookingSkillPath = GetCookingSkillModPath();
-                var fileStream = new FileStream(Path.Combine(cookingSkillPath, IconFileName), FileMode.Open);
-                _cookingSkillTexture = Texture2D.FromStream(Game1.graphics.GraphicsDevice, fileStream);
-            }
-            catch (Exception ex)
-            {
-                Log.Async("[Skill Prestige Cooking Adapter] failed to load icon: " + ex);
-                _cookingSkillTexture = new Texture2D(Game1.graphics.GraphicsDevice, 16, 16);
-                _cookingSkillTexture.SetData(Enumerable.Range(0, 256).Select(i => new Color(225, 168, byte.MaxValue)).ToArray());
-            }
-            return _cookingSkillTexture;
-        }
+        /// <summary>
+        /// The cooking skill icon.
+        /// </summary>
+        internal static Texture2D IconTexture;
 
-        private static string GetCookingSkillModPath()
+        /// <summary>
+        /// Whether the Cooking Skill mod is loaded.
+        /// </summary>
+        internal static bool IsCookingSkillModLoaded;
+
+        /// <summary>
+        /// Whether the Luck Skill mod is loaded.
+        /// </summary>
+        internal static bool IsLuckSkillModLoaded;
+
+        public override void Entry(IModHelper helper)
         {
-            var type = typeof(Program);
-            var info = type.GetField("_modPaths", BindingFlags.NonPublic | BindingFlags.Static);
-            var modPaths = (List<string>)info.GetValue(null);
-            foreach (var modPath in modPaths)
-            {
-                foreach (var directory in Directory.GetDirectories(modPath))
-                {
-                    foreach (var file in Directory.GetFiles(directory, "manifest.json"))
-                    {
-                        if (file.Contains("StardewInjector")) continue;
-                        var baseConfig = new Manifest();
-                        Manifest manifest;
-                        try
-                        {
-                            if (string.IsNullOrEmpty(File.ReadAllText(file)))
-                            {
-                                continue;
-                            }
-                            manifest = baseConfig.InitializeConfig(file);
-                            if (string.IsNullOrEmpty(manifest.EntryDll))
-                            {
-                                continue;
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-                        var directoryName = Path.GetDirectoryName(file);
-                        if (manifest.UniqueID == "CookingSkill")
-                        {
-                            return directoryName;
-                        }
-                    }
-                }
-            }
-            return string.Empty;
+            IconTexture = helper.Content.Load<Texture2D>("icon.png");
+            IsCookingSkillModLoaded = helper.ModRegistry.IsLoaded("spacechase0.CookingSkill");
+            IsLuckSkillModLoaded = helper.ModRegistry.IsLoaded("spacechase0.LuckSkill");
         }
     }
 }
